40eae2b4d04dd5819b81d888d023d8211f7df55a
lnwLeezaza <maykop908@gmail.com>
2025-11-16 12:53:18 +0700

    remove move www.

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://www.alltoolsz.online"
+  const baseUrl = "https://alltoolsz.online"
 
   return {
     rules: [
